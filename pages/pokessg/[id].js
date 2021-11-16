f1e14e9250113109e85677fcff017784b9580c1f
AipBorges <mpbds@uic.es>
2021-11-16 21:44:08 +0100

    started the id function for details

---
+++ pages/pokessg/[id].js
@@ -0,0 +1,31 @@
+const Pokemons = ({ pokemons }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <p>loading</p>;
+  }
+
+  return html;
+};
+
+export const getStaticPaths = async () => {
+  const response = await fetch("https://pokeapi-menchu.herokuapp.com/pokemon");
+  const pokeAPI = await response.json();
+
+  const paths = pokeApi.map((pokemon) => ({ params: { id: `${pokemon.id}` } }));
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async (params) => {
+  const response = await fetch(
+    "https://pokeapi-menchu.herokuapp.com/pokemon" + id
+  );
+  const pokeAPI = await response.json();
+  return {
+    props: {
+      pokemons: pokeApi,
+    },
+  };
+};
