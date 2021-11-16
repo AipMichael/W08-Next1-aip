7c473a2a26da272a57ef2dde18fa101aa785e81b
AipBorges <mpbds@uic.es>
2021-11-16 19:45:09 +0100

    added styles to pokemon page

--- pages/pokemon/index.js
+++ pages/pokemon/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Link from "next/link";
+import styles from "../../styles/pokemon.module.css";
 
 export default function Pokemon() {
   const [pokemons, setPokemons] = useState([]);
@@ -17,6 +19,19 @@ export default function Pokemon() {
   );
   return (
     <>
+      <header className={styles.headerBar}>
+        <h1 className={styles.title}>
+          Poke-hola. Esto es de Client Server Rendering
+        </h1>
+        <div className={styles.headerMenu}>
+          <Link href="/pokemon" cllinkssName={styles.headRoutes}>
+            Pokémon
+          </Link>
+          <h2 className={styles.headRoutes}>Poke SSR</h2>
+          <h2 className={styles.headRoutes}>Poke SSG</h2>
+          <h2 className={styles.headRoutes}>Poke ISR</h2>
+        </div>
+      </header>
       <ul>
         {pokemons.map(({ name }) => (
           <li key={name}>{name}</li>
